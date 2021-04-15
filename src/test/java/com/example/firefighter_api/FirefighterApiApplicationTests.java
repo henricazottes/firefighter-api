a444a4b98ed33dcac2aca6d4c5e4354e65a82eb6
Henri Cazottes <henri.cazottes@protonmail.com>
2021-04-15 21:49:05 +0200

    Initial commit

---
+++ src/test/java/com/example/firefighter_api/FirefighterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firefighter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirefighterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
